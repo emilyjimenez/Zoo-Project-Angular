3a3a4d068f7c9f868376bc8d9eb77eddcb447db2
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-12-08 14:42:28 -0800

    begin building zoo class and components.

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Hyrule Zoo</h1>
+  </div>
+  `
+})
+
+export class AppComponent { }
